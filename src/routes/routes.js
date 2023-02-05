56ce1f864436873c7b37031f638c97ea79b1a7e7
boldak <boldak.andrey@gmail.com>
2023-02-05 22:08:56 +0200

    html output

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,32 +6,51 @@ const toHtml = (org, data) => {
 
 	const rowMapper = d => {
 		return `
-			<div><strong>${d.examination}</strong><div>
-			<div><pre>${d.validation}</pre></div>
+			<div class="title">${d.examination}</div>
+			<div><pre class="subtitle-2">${d.validation}</pre></div>
 		`
 	}
 
+
 	return `
-		<h1> DATA VALIDATION REPORT </h1>
-		<h2> Company: "${org}" Date:${moment(new Date()).format("YYYY-MM-DD HH:mm:ss")}</h2>
-		${data.map( d => rowMapper(d))} 
+		<html>
+			<head>
+			  <meta charset="utf-8">
+			  <meta http-equiv="x-ua-compatible" content="ie=edge">
+			  <meta name="description" content="">
+			  <meta name="viewport" content="width=device-width, initial-scale=1">
+			  <link rel="stylesheet" href="../../../../vuetify.min.css" />
+			</head>
+
+			<body>
+				<div class="app">
+					<div class="v-application v-application--is-ltr theme--light">
+						<div class="v-application--wrap">
+							
+							<div class="ma-4">
+								<div class="d-flex align-center pb-5">
+									<div class="display-1">DATA VALIDATION REPORT</div>
+									<div class="spacer"></div>
+									<div class="pa-5 ma-3 subtitle-2  font-weight-bold" style="border:1px solid;">Company: "${org}"<br/>Date: ${moment(new Date()).format("YYYY-MM-DD HH:mm:ss")}</div>
+								</div>
+								${data.map( d => rowMapper(d))}
+							</div>
+						</div>
+					</div>
+				</div>		
+			</body>
+		</html>		 
 	`	
 }
 
 
-router.get("/validate/:org/:type", (req, res) => {
-	
-	console.log("/validate/:org/:type", req.params)
+const validate = (res, organization, type) => {
 
-	let organization = req.params.org
-	let type = req.params.type || "html"
-	type = (type != "html") ? "json" : "html"
-	
 	if(organization){
 	  const child = fork("./sync-data/src/childs/validate-data")
 	  
 	  child.on('message', result => {
-	    console.log('Parent process received:', result.data);
+	    // console.log('Parent process received:', result.data);
 	    if( type == "json" ){
 	    	res.send(result.data)
 	    } else {
@@ -52,7 +71,28 @@ router.get("/validate/:org/:type", (req, res) => {
 	
 	} else {
 		res.status(404).send("Organization data validation error: Organization undefined.")
-	}  
+	} 
+
+}
+
+
+router.get("/validate/:org/:type", (req, res) => {
+	
+	let organization = req.params.org
+	let type = req.params.type || "html"
+	type = (type != "html") ? "json" : "html"
+	
+	validate(res, organization, type)
+ 
+})
+
+router.get("/validate/:org", (req, res) => {
+	
+	let organization = req.params.org
+	let type = "html"
+	
+	validate(res, organization, type)
+ 
 })
 
 router.get("/sync", (req, res) => {
