8215f807cb06a4d9d028e1273d6c5c0af2aa5ce2
boldak <boldak.andrey@gmail.com>
2023-06-20 23:17:49 +0300

    update validate-job.sh

--- validate-job.sh
+++ validate-job.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo 'Start validation job for'  $1
 cd /home/andrii-boldak/projects/jace/sync-data 
-/home/andrii-boldak/.nvm/versions/node/v14.13.1/bin/node ./src/scripts/validate-data $1
+/home/andrii-boldak/.nvm/versions/node/v14.13.1/bin/node ./src/scripts/validate-data1 $1
