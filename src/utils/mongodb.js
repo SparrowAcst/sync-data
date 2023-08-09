9da23adcf414b734fb6e90c818b76473ffa329db
boldak <boldak.andrey@gmail.com>
2023-08-09 20:35:12 +0300

    fix client.close

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -36,93 +36,203 @@ const normalize = str => {
 
 const aggregate = async (collectionName, pipeline) => {
 	
-	let client = await getClient()
+	let client
+	
+	try {
+		
+		client = await getClient()
 
-	let conf = normalize(collectionName)
-	let db = client.db(conf.dbName)
-    let collection = db.collection(conf.collectionName)
-    pipeline = pipeline || []
-    let res = await collection.aggregate(pipeline.concat([{$project:{_id:0}}])).toArray()
-    return res
+		let conf = normalize(collectionName)
+		let db = client.db(conf.dbName)
+	    let collection = db.collection(conf.collectionName)
+	    pipeline = pipeline || []
+	    let res = await collection.aggregate(pipeline.concat([{$project:{_id:0}}])).toArray()
+	    return res
+	
+	} catch (e) {
+	
+		throw e
+	
+	} finally {
+	
+		if (client)  client.close()
+	
+	}    
 }
 
 const getAggregateCursor =  async (collectionName, pipeline) => {
-	
-	let client = await getClient()
 
-	let conf = normalize(collectionName)
-	let db = client.db(conf.dbName)
-    let collection = db.collection(conf.collectionName)
-    pipeline = pipeline || []
-    let res = collection.aggregate(pipeline.concat([{$project:{_id:0}}]))
-    return res
+	let client
+	
+	try {
+		
+		client = await getClient()
+
+		let conf = normalize(collectionName)
+		let db = client.db(conf.dbName)
+	    let collection = db.collection(conf.collectionName)
+	    pipeline = pipeline || []
+	    let res = collection.aggregate(pipeline.concat([{$project:{_id:0}}]))
+	    return res
+
+	} catch (e) {
+	
+		throw e
+	
+	} finally {
+	
+		if (client)  client.close()
+	
+	}     
 }
 
 const removeAll = async (collectionName) => {
 	
-	let client = await getClient()
+	let client
+	
+	try {
+		
+		client = await getClient()
 
-	let conf = normalize(collectionName)
-	let db = client.db(conf.dbName)
-    let collection = db.collection(conf.collectionName)
-    await collection.deleteMany({})
+		let conf = normalize(collectionName)
+		let db = client.db(conf.dbName)
+	    let collection = db.collection(conf.collectionName)
+	    await collection.deleteMany({})
+	
+	} catch (e) {
+	
+		throw e
+	
+	} finally {
+	
+		if (client)  client.close()
+	
+	}	    
 } 
 
 const insertAll = async (collectionName, data) => {
 	
-	let client = await getClient()
+	let client
+	
+	try {
+		
+		client = await getClient()
 
-	let conf = normalize(collectionName)
-	let db = client.db(conf.dbName)
-    let collection = db.collection(conf.collectionName)
+		let conf = normalize(collectionName)
+		let db = client.db(conf.dbName)
+	    let collection = db.collection(conf.collectionName)
 
-	await collection.insertMany(data)
+		await collection.insertMany(data)
 
+	} catch (e) {
+	
+		throw e
+	
+	} finally {
+	
+		if (client)  client.close()
+	
+	}	
 }
 
 const bulkWrite = async (collectionName, commands) => {
 	
-	let client = await getClient()
+	let client
+	
+	try {
+		
+		client = await getClient()
 
-	let conf = normalize(collectionName)
-	let db = client.db(conf.dbName)
-    let collection = db.collection(conf.collectionName)
-	await collection.bulkWrite(commands)
+		let conf = normalize(collectionName)
+		let db = client.db(conf.dbName)
+	    let collection = db.collection(conf.collectionName)
+		await collection.bulkWrite(commands)
+
+	} catch (e) {
+	
+		throw e
+	
+	} finally {
+	
+		if (client)  client.close()
+	
+	}	
 }
 
 const replaceOne = async (collectionName, filter, data) => {
 	
-	let client = await getClient()
+	
+	let client
+	
+	try {
+		
+		client = await getClient()
 
-	let conf = normalize(collectionName)
-	let db = client.db(conf.dbName)
-    let collection = db.collection(conf.collectionName)
-    await collection.replaceOne(filter, data, {upsert: true})
-
+		let conf = normalize(collectionName)
+		let db = client.db(conf.dbName)
+	    let collection = db.collection(conf.collectionName)
+	    await collection.replaceOne(filter, data, {upsert: true})
+	} catch (e) {
+	
+		throw e
+	
+	} finally {
+	
+		if (client)  client.close()
+	
+	}    
 }
 
 const updateOne = async (collectionName, filter, data) => {
 
-	let client = await getClient()
+	let client
+	
+	try {
+		
+		client = await getClient()
 
-	let conf = normalize(collectionName)
-	let db = client.db(conf.dbName)
-    let collection = db.collection(conf.collectionName)
-    await collection.updateOne(filter, { $set:data }, { upsert: true })
+		let conf = normalize(collectionName)
+		let db = client.db(conf.dbName)
+	    let collection = db.collection(conf.collectionName)
+	    await collection.updateOne(filter, { $set:data }, { upsert: true })
 
+	} catch (e) {
+	
+		throw e
+	
+	} finally {
+	
+		if (client)  client.close()
+	
+	}        
 }
 
 const listCollections = async dbSchema => {
 
-	let client = await getClient()
+	let client
+	
+	try {
+	
+		let client = await getClient()
 
-		
-	let conf = normalize(dbSchema)
-	const res =  await client
-					.db(conf.dbName)
-    				.listCollections()
-    				.toArray()
-	return res
+			
+		let conf = normalize(dbSchema)
+		const res =  await client
+						.db(conf.dbName)
+	    				.listCollections()
+	    				.toArray()
+
+		return res
+	
+	} catch (e) {
+	
+		throw e
+	
+	} finally {
+	
+		if (client)  client.close()
+	
+	}		
 	
 }
 
